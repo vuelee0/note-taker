9eaa31b130ae8f05356e9fe9fc2842667ed3b0b7
Vue Lee <vuelee09@gmail.com>
2022-05-31 00:38:37 -0500

    route to serve note.html

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+module.exports = router;
