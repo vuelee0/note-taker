bad83ccd9ba38ceadc7373781c07dd788e97fe51
Vue Lee <vuelee09@gmail.com>
2022-06-01 00:55:11 -0500

    get, post, delete routes

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { noteCreate, noteDelete } = require('../../lib/notes');
+
+router.get('/notes', (req, res) => {
+    let newNote = notes;
+    res.json(newNote);
+})
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    let note = noteCreate(req.body, notes);
+    res.json(note);
+})
+
+router.delete('/notes/:id', (req, res) => {
+    noteDelete(notes, req.params.id);
+    res.json(notes);
+})
+
+module.exports = router;
